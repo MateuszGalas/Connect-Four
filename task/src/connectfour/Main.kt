8822d7ede6e949af8f8674d2c58b0ff79a1877fb
Mateusz Galaś <mateuszgalas94@gmail.com>
2022-12-03 21:31:33 +0100

    First task, initial form of game.

--- task/src/connectfour/Main.kt
+++ task/src/connectfour/Main.kt
@@ -1,5 +1,45 @@
 package connectfour
 
 fun main() {
-    println("Hello World!")
+    var rowsAndColumns: String
+    var rows: Int
+    var columns: Int
+
+    println("Connect Four")
+    println("First player's name:")
+    val firstPlayer = readln()
+    println("Second player's name:")
+    val secondPLayer = readln()
+
+    while (true) {
+        println("Set the board dimensions (Rows x Columns)\n" +
+                "Press Enter for default (6 x 7)")
+        rowsAndColumns = readln()
+        rowsAndColumns = rowsAndColumns.filter { !it.isWhitespace() }
+        rowsAndColumns = rowsAndColumns.lowercase()
+
+        if (rowsAndColumns.isEmpty()) {
+            rows = 6
+            columns = 7
+        } else if (!rowsAndColumns.matches("""\d+x\d+""".toRegex())) {
+            println("Invalid input")
+            continue
+        } else {
+            rows = rowsAndColumns.split("x").first().toInt()
+            columns = rowsAndColumns.split("x").last().toInt()
+        }
+
+        if (rows !in 5..9) {
+            println("Board rows should be from 5 to 9")
+            continue
+        } else if (columns !in 5..9) {
+            println("Board columns should be from 5 to 9")
+            continue
+        } else {
+            break
+        }
+    }
+
+    println("$firstPlayer vs $secondPLayer")
+    println("$rows X $columns board")
 }
